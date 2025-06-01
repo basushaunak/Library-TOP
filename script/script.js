c89f2103ad910343b1b8c2aef495c793ccff1354
Shaunak Basu <55021457+basushaunak@users.noreply.github.com>
2025-06-01 13:44:13 +0530

    WIP

--- script/script.js
+++ script/script.js
@@ -14,9 +14,10 @@
 const btnShowBookAddDialog = document.querySelector("#btn-show-book-add-dialog");
 const btnAddBook = document.querySelector("#btn-add-book");
 const btnCancel = document.querySelector("#btn-cancel");
-
+// const btnRemoveBook = document.querySelectorAll(".btn-remove-book");
 
 const dialogAddBook = document.querySelector("#dialog-add-book");
+const bookGrid = document.querySelector("#book-grid");
 
 const title = document.querySelector("#title");
 const author = document.querySelector("#author");
@@ -29,65 +30,68 @@ const isRead = document.querySelector("#is-read");
 const myLibrary = [];
 
 
-
-
 function getLibraryId(){
     return crypto.randomUUID();
 }
 
-function Book(libraryId,title,author,pages,isRead,rating) {
+//libId,title.value,author.value,pages.value,rating.value,isRead.value
+function Book(libraryId,title,author,pages,rating, isRead) {
     this.libraryId = libraryId;
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.isRead = isRead;
     this.rating = rating;
+    this.isRead = isRead;
 }
 
-function updatePage(){
+function updateBookGrid(){
     if(myLibrary.length === 0){
         bookGrid.innerHTML = "";
         return ;
     }
     let htmlStr = ``; 
+    bookGrid.innerHTML=``;
     for(let i=0;i<myLibrary.length;i++){
-        htmlStr = `<div clas="book-card" id = "${myLibrary[i].libraryId}">
+        let checked="";
+        if(myLibrary[i].isRead){
+            checked="checked";
+        }
+        htmlStr = `<div class="book-card">
                 <h2>${myLibrary[i].title}</h2>
                 <p>by ${myLibrary[i].author}</p>
                 <p>Total Pages: ${myLibrary[i].pages}</p>
                 <p>Rating: ${myLibrary[i].rating}/10</p>
-                <input style="checkbox" id="chk-${myLibrary[i].libraryId}" value = "${myLibrary[i].isRead}">
-                <label for ="chk-${myLibrary[i].libraryId}">Already Read</label>
-                <btn id="btn-remove-book">Remove</btn>
+                <div>
+                    <input type="checkbox" id="chk-${myLibrary[i].libraryId}"  ${checked}>
+                    <label for ="chk-${myLibrary[i].libraryId}">Already Read</label>
+                </div>
+                <button class="btn-remove-book" id="${myLibrary[i].libraryId}">Remove</btn>
                 </div>`
-        
-
+        bookGrid.innerHTML+=htmlStr;
+        htmlStr=``;
     }
 }
 
 function addBookToLibrary() {
     let libId = getLibraryId();
-    let book = new Book(libId);
+    let book = new Book(libId,title.value,author.value,pages.value,rating.value,isRead.checked);
     myLibrary.push(book);
-    updatePage();
+    // updateBookGrid();
 }
 
 function removeBookFromLibrary(libraryId){  
     //Remove from array
-    updatePage();  
+    updateBookGrid();  
 }
 
 function isValidFormData(){
     if (title.value.trim().length === 0 ){
-        alert("Title: " + title.value.trim().length);
         return false;
     }
     if (author.value.trim().length === 0 ){
-        alert("author");
         return false;
     }
     if(Number(rating.value) < 1 || Number(rating.value) > 10){
-        alert("rating");
         return false;
     }
     return true;
@@ -100,8 +104,8 @@ btnAddBook.addEventListener("click", (e)=>{
     }
     dialogAddBook.hidden=true;
     btnShowBookAddDialog.style.visibility = 'visible';
-
     addBookToLibrary();
+    updateBookGrid();
 })
 
 btnShowBookAddDialog.addEventListener("click",(e)=>{
@@ -124,3 +128,8 @@ function initDialog(){
     rating.value="5";
     isRead.checked = false;
 }
+
+bookGrid.addEventListener("click",(e)=>{
+    alert(e.target.id);
+    alert(e.target.nodeName);
+})
